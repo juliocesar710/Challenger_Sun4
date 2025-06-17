b55fed6d638784603fd3b4461efe256fd94137fd
julio candeia <juliocandeia594@gmail.com>
2025-06-17 12:44:39 -0300

    feat: completed challenge 4

--- sun4-desafios/src/pages/Question_4/Q4Page.tsx
+++ sun4-desafios/src/pages/Question_4/Q4Page.tsx
@@ -1,3 +1,34 @@
-export default function Q4Page() {
-  return <div>Q4Page</div>;
-}
+import React from "react";
+import '../../index.css'
+
+
+const LayoutResponsivo: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Cabeçalho */}
+      <header className="bg-blue-700 text-white text-center p-4 text-xl font-bold">
+        Cabeçalho
+      </header>
+
+      {/* Conteúdo principal */}
+      <main className="flex flex-col md:flex-row flex-1">
+        <div className="flex-1 p-4 bg-blue-100 border md:border-r-2 border-blue-300 text-center">
+          Coluna 1
+        </div>
+        <div className="flex-1 p-4 bg-blue-200 border md:border-r-2 border-blue-300 text-center">
+          Coluna 2
+        </div>
+        <div className="flex-1 p-4 bg-blue-300 border border-blue-300 text-center">
+          Coluna 3
+        </div>
+      </main>
+
+      {/* Rodapé */}
+      <footer className="bg-blue-700 text-white text-center p-4 text-sm">
+        Rodapé
+      </footer>
+    </div>
+  );
+};
+
+export default LayoutResponsivo;
